7d25b10a41e0cf30f029799bd790b61cff4d1adf
Khánh Đinh <dgkhanh2003@gmail.com>
2024-03-11 21:24:26 +0700

    filter

--- src/apis/filter.js
+++ src/apis/filter.js
@@ -3,17 +3,17 @@ import { makeRequest } from "../axios";
 const filterAPI = {
     getAllProducts: async ({ name, latest, mostSold, lowestPrice, highestPrice, priceRange, color, size }) => {
         try {
-            // const queryParams = new URLSearchParams({
-            //     name: name || "",
-            //     latest: latest || "",
-            //     mostSold: mostSold || "",
-            //     lowestPrice: lowestPrice || "",
-            //     highestPrice: highestPrice || "",
-            //     priceRange: priceRange || "",
-            //     color: color || "",
-            //     size: size || ""
-            // });
-            const queryParams = `name=${name || ""}&latest=${latest || ""}&mostSold=${mostSold || ""}&lowestPrice=${lowestPrice || ""}&highestPrice=${highestPrice || ""}&priceRange=${priceRange || ""}&color=${color || ""}&size=${size || ""}`;
+            const queryParams = new URLSearchParams({
+                name: name || "",
+                latest: latest || "",
+                mostSold: mostSold || "",
+                lowestPrice: lowestPrice || "",
+                highestPrice: highestPrice || "",
+                priceRange: priceRange || "",
+                color: color || "",
+                size: size || ""
+            });
+            const response = await makeRequest.get(`/api/v1/products/filter?${queryParams}`);
             return response.data;
         } catch (error) {
             return error?.response?.data;
